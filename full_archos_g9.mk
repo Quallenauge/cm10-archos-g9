cce1eb9050bfc1d0927b9379975145b16875e11a
Marco Brohet <therbom@gmail.com>
2013-04-27 22:17:04 +0200

    Add support for A101S

--- full_archos_g9.mk
+++ full_archos_g9.mk
@@ -26,12 +26,15 @@ endif
 PRODUCT_COPY_FILES := \
 	$(LOCAL_KERNEL):kernel \
 	$(PREBUILT_PATH)/root/fstab.archosa80sboard:root/fstab.archosa80sboard \
+	$(PREBUILT_PATH)/root/fstab.archosa101sboard:root/fstab.archosa80sboard \
 	$(PREBUILT_PATH)/root/init.archos_init.rc:root/init.archos_init.rc \
 	$(PREBUILT_PATH)/root/init.archosa80sboard.rc:root/init.archosa80sboard.rc \
+	$(PREBUILT_PATH)/root/init.archosa101sboard.rc:root/init.archosa80sboard.rc \
 	$(PREBUILT_PATH)/root/init.dongle.rc:root/init.dongle.rc \
 	$(PREBUILT_PATH)/root/init.goldfish.rc:root/init.goldfish.rc \
 	$(PREBUILT_PATH)/root/init.rc:root/init.rc \
 	$(PREBUILT_PATH)/root/ueventd.archos.rc:root/ueventd.archosa80sboard.rc \
+	$(PREBUILT_PATH)/root/ueventd.archos.rc:root/ueventd.archosa101sboard.rc \
 	$(PREBUILT_PATH)/root/ueventd.goldfish.rc:root/ueventd.goldfish.rc \
 	$(PREBUILT_PATH)/root/ueventd.rc:root/ueventd.rc
 
