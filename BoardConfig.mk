cfc16fee43d35b586e3e4a7ba97e286bd9d2bffb
Quallenauge <Hamsi2k@freenet.de>
2014-01-05 22:26:13 +0100

    Trying to get closer to the reference implementation of KK external sdcard mount system.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -104,11 +104,7 @@ TARGET_BOOTLOADER_BOARD_NAME := archos
 
 BOARD_EGL_CFG := $(PREBUILT_PATH)/system/lib/egl/egl.cfg
 
-#BOARD_USES_HGL            := true
-#BOARD_USES_OVERLAY        := true
 USE_OPENGL_RENDERER        := true
-#BOARD_USES_PANDA_GRAPHICS := true
-
 # Causes out of memory exceptions
 #NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
 #Refer to frameworks/native/libs/gui/SurfaceComposerClient.cpp
@@ -118,11 +114,9 @@ COMMON_GLOBAL_CFLAGS += -DFORCE_SCREENSHOT_CPU_PATH
 BOARD_USES_HWCOMPOSER := true                                                                                                                                                                                                                                                  
 # BOARD_USE_SYSFS_VSYNC_NOTIFICATION := true
 # set if the target supports FBIO_WAITFORVSYNC
-TARGET_HAS_WAITFORVSYNC := true
+# TARGET_HAS_WAITFORVSYNC := true
 # This is implemented since kernel 3.10.0 
 TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
-# Handled in frameworks/native/opengl/libs/Android.mk ???
-BOARD_EGL_WORKAROUND_BUG_10194508 := true
 
 BOARD_RIL_NO_CELLINFOLIST  := true
 
@@ -140,7 +134,7 @@ ifdef OMAP_ENHANCEMENT_MULTIGPU
     COMMON_GLOBAL_CFLAGS += -DOMAP_ENHANCEMENT_MULTIGPU
 endif
 
-# Audio library - as long microphone doesn't work with upstream drivers
+# Audio library - not needed anymore
 # COMMON_GLOBAL_CFLAGS += -DICS_AUDIO_BLOB
 # AUDIO_POLICY_TEST := true
 
@@ -149,7 +143,11 @@ endif
 # Enable dalvik startup with a low memory footprint
 TARGET_ARCH_LOWMEM := true
 
-TARGET_USERIMAGES_USE_EXT4         := true
+# Filesystem
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_FLASH_BLOCK_SIZE := 4096
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/virtual/android_usb/android0/f_mass_storage/lun/file"
+
 # Use 384MByte as system image
 BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 402653184
 #BOARD_SYSTEMIMAGE_PARTITION_SIZE  := 16777216
@@ -159,25 +157,31 @@ BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE  := ext4
 BOARD_FLASH_BLOCK_SIZE             := 4096
 
 # Connectivity - Wi-Fi
-#USES_TI_MAC80211 := true
+USES_TI_MAC80211 := true
+
+ifdef USES_TI_MAC80211
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wl12xx
+BOARD_HOSTAPD_DRIVER_TI := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_wl12xx
+PRODUCT_WIRELESS_TOOLS := true
+BOARD_WLAN_DEVICE := wl12xx_mac80211
+BOARD_SOFTAP_DEVICE := wl12xx_mac80211
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/wl12xx_sdio.ko"
+WIFI_DRIVER_MODULE_NAME := "wl12xx_sdio"
+WIFI_FIRMWARE_LOADER := ""
+COMMON_GLOBAL_CFLAGS += -DUSES_TI_MAC80211
+
+else
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wl12xx
 BOARD_WLAN_DEVICE := wl12xx_mac80211
 WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/wl12xx_sdio.ko"
 WIFI_DRIVER_MODULE_NAME := "wl12xx_sdio"
 WIFI_FIRMWARE_LOADER := ""
-
-ifdef USES_TI_MAC80211
-#BOARD_SOFTAP_DEVICE := wl12xx_mac80211
-#BOARD_HOSTAPD_DRIVER := NL80211
-#BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_wl12xx
-
-WPA_SUPPLICANT_VERSION := VER_0_8_X_TI
-BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_wl12xx
-PRODUCT_WIRELESS_TOOLS := true
-COMMON_GLOBAL_CFLAGS += -DUSES_TI_MAC80211
-else
 WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_HOSTAPD_DRIVER := NL80211
 endif
 
 #TARGET_PROVIDES_INIT_RC := true
