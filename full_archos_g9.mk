d8bf8ebfd8bc37280f9861fd8ac0c8abca19c5f5
Quallenauge <Hamsi2k@freenet.de>
2014-03-11 19:54:43 +0100

    Reduce scan intervall to 15 seconds. Maybe this helps with stock wpa_supplicant.

--- full_archos_g9.mk
+++ full_archos_g9.mk
@@ -296,7 +296,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     wifi.interface=wlan0 \
-    wifi.supplicant_scan_interval=120 \
+    wifi.supplicant_scan_interval=15 \
     ro.opengles.version=131072 \
     com.ti.omap_enhancement=true \
     omap.enhancement=true \
