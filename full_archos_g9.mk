1b51b5161468211136ded5d4abbf1f575995a219
Quallenauge <Hamsi2k@freenet.de>
2015-03-14 22:27:57 +0100

    omap4: use L3 widevine libs from oppo/msm8974-common

--- full_archos_g9.mk
+++ full_archos_g9.mk
@@ -68,9 +68,12 @@ PRODUCT_COPY_FILES += \
 
 # DRM Proprietary Files
 PRODUCT_COPY_FILES += \
+	$(PREBUILT_PATH)/system/lib/libdrmdecrypt.so:system/lib/libdrmdecrypt.so \
+        $(PREBUILT_PATH)/system/vendor/lib/drm/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so \
+	$(PREBUILT_PATH)/system/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so \
 	$(PREBUILT_PATH)/system/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so \
 	$(PREBUILT_PATH)/system/vendor/lib/libwvdrm_L3.so:system/vendor/lib/libwvdrm_L3.so \
-	$(PREBUILT_PATH)/system/vendor/lib/libWVStreamControlAPI_L3.so:system/vendor/lib/libWVStreamControlAPI_L3.so \
+	$(PREBUILT_PATH)/system/vendor/lib/libWVStreamControlAPI_L3.so:system/vendor/lib/libWVStreamControlAPI_L3.so
         
         
 # Vold Mounting
